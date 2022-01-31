39050e75289446e1ce4201439193b61df6de2c0b
tamil <tamilvicky17@gmail.com>
2022-01-31 13:19:03 +0530

    last update

--- lib/src/cluster.dart
+++ lib/src/cluster.dart
@@ -4,6 +4,9 @@ import 'package:google_maps_flutter_platform_interface/google_maps_flutter_platf
 class Cluster<T extends ClusterItem> {
   final LatLng location;
   final Iterable<T> items;
+  // final String course;
+  // final String icon;
+  // final String rotation;
 
   Cluster(this.items)
       : this.location = LatLng(
@@ -17,6 +20,24 @@ class Cluster<T extends ClusterItem> {
 
   /// True if cluster is not a single item cluster
   bool get isMultiple => items.length > 1;
+
+  String _course;
+  set course(String value) {
+    _course = value;
+  }
+  String get course => _course;
+
+  String _icon;
+  set icon(String value) {
+    _icon = value;
+  }
+  String get icon => _icon;
+
+  String _rotation;
+  set rotation(String value) {
+    _rotation = value;
+  }
+  String get rotation => _rotation;
 
   /// Basic cluster marker id
   String getId() {
