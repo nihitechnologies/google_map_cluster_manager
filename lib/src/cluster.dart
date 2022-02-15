42a4ec451fea92c4416d8f42f903509aa373ff31
tamil <tamilvicky17@gmail.com>
2022-02-15 11:29:14 +0530

    last update

--- lib/src/cluster.dart
+++ lib/src/cluster.dart
@@ -36,7 +36,7 @@ class Cluster<T extends ClusterItem> {
 
   String get text => items.fold<String>("", (p, c) => p + c.text);
 
-  dynamic get device=> items.fold<dynamic>("", (p, c) => p + c);
+  dynamic get device=> items.fold<dynamic>("", (p, c) => p + c.device);
 
 
   @override
